9ac5e76224c5532c0ab4edb9e72d9be54fa00f2e
Donny <107092000+Donny-Guo@users.noreply.github.com>
2025-06-09 21:10:07 -0700

    Add conditional rendering on game status

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,59 @@ import { languages } from "./languages.js"
 import clsx from 'clsx'
 
 export default function App() {
+  // State values
   const [currentWord, setCurrentWord] = useState('react')
   const [selectedLetters, setSelectedLetters] = useState([])
-  
+
+  // Derived values
   const wrongGuessCount = selectedLetters.reduce((res, letter) => {
     return !currentWord.includes(letter) ? res + 1 : res
   }, 0)
+  const isGameWon = 
+    currentWord.split("").every(letter => selectedLetters.includes(letter))
+  const isGameLost = wrongGuessCount >= languages.length
+  const isGameOver = isGameWon || isGameLost
 
-  const isWon = false
+  // Static values
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
   const gameStatusElement = (
-    <>
-      <h3>You win!</h3>
-      <p>Well done! 🎉</p>
-    </>
-  )
+    isGameOver ? (
+      isGameWon ? (
+        <>
+          <h3>You win!</h3>
+          <p>Well done! 🎉</p>
+        </>
+      ) : (
+        <>
+          <h3>Game over!</h3>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      )
+     ) : (
+      null
+  ))
 
   const languageChipsElement = languages.map((language, index) => {
-      const styles = {
-        backgroundColor: language.backgroundColor,
-        color: language.color,
-      }
-      const className = clsx({
-        chip: true,
-        lost: index < wrongGuessCount,
-      })
-      
-      return (
-        <div 
-          className={className}
-          key={language.name} 
-          style={styles} 
-        >
-          {language.name}
-        </div>
-      )
+    const styles = {
+      backgroundColor: language.backgroundColor,
+      color: language.color,
     }
+    const className = clsx({
+      chip: true,
+      lost: index < wrongGuessCount,
+    })
+
+    return (
+      <div
+        className={className}
+        key={language.name}
+        style={styles}
+      >
+        {language.name}
+      </div>
+    )
+  }
   )
 
   const lettersElement = [...currentWord].map((letter, index) => (
@@ -47,8 +64,7 @@ export default function App() {
     </div>
   ))
 
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  const keyboardElements = alphabet.split('').map(letter => { 
+  const keyboardElements = alphabet.split('').map(letter => {
     const isSelected = selectedLetters.includes(letter)
     const isCorrect = isSelected && currentWord.includes(letter)
     const isWrong = isSelected && !currentWord.includes(letter)
@@ -58,9 +74,9 @@ export default function App() {
       'keyboard-letter': true,
     })
 
-    return(
-      <button key={letter} 
-        className={className} onClick={()=> handleClick(letter)}
+    return (
+      <button key={letter}
+        className={className} onClick={() => handleClick(letter)}
       >
         {letter.toUpperCase()}
       </button>
@@ -68,10 +84,16 @@ export default function App() {
   })
 
   function handleClick(letter) {
-    if (!selectedLetters.includes(letter)) {
+    if (!selectedLetters.includes(letter) && !isGameOver) {
       setSelectedLetters(prev => [...prev, letter])
     }
   }
+
+  const gameStatusClass = clsx({
+    "game-status": true,
+    won: isGameWon,
+    lost: isGameLost,
+  })
 
   return (
     <main>
@@ -85,7 +107,7 @@ export default function App() {
       </header>
 
 
-      <section className="game-status">
+      <section className={gameStatusClass}>
         {gameStatusElement}
       </section>
 
@@ -102,11 +124,12 @@ export default function App() {
       </section>
 
       <section className='new-game-section'>
-        <button className='new-game-button'>
+        {isGameOver && <button className='new-game-button'>
           New Game
         </button>
+        }
       </section>
-      
+
     </main>
   )
 }
